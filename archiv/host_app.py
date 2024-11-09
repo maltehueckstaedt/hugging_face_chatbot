bb05ca44bfcd2895fb8abc7704d388f587604a4a
Malte Hückstädt <malte.hueckstaedt@stifterverband.de>
2024-11-09 21:15:37 +0100

    remove old files

--- archiv/host_app.py
+++
@@ -1,8 +0,0 @@
-# Registieren:
-
-#pip install rsconnect-python
-
-rsconnect add --account chernofffaces --name chernoffchat --token 00FE4DB06B18C441104871FF5DDF89F0 --secret /VMfR87jyuteccR8rpeERDeou+MjgDKkZ/DU/tAs
-
-
-rsconnect deploy shiny app --name chernoffchat --title Chernoff.Chat
